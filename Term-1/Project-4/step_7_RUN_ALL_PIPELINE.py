c0f13839f5de7f737d28fbfe629a3cf09ca6033d
AlexSickert <alex@alexandersickert.com>
2017-02-03 23:40:12 +0200

    initial version

---
+++ Term-1/Project-4/step_7_RUN_ALL_PIPELINE.py
@@ -0,0 +1,10 @@
+"""
+ -------------------------------------------------------------
+   This file tests the entire pipeline, meaning: 
+   1. calibrate
+   2. test distortion correction
+   3. step 3 - 6 on an image
+   4. run step 3 to 6 on a video
+ -------------------------------------------------------------
+"""
+print(1)
