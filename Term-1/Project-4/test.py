5120b4212edcb54913d7768f476a5dd9b8ed694f
AlexSickert <alex@alexandersickert.com>
2017-02-03 23:30:50 +0200

    initial version

--- Term-1/Project-4/test.py
+++ Term-1/Project-4/test.py
@@ -1,9 +1,15 @@
-###
 
+# -------------------------------------------------------------
+#   This file tests the entire pipeline, meaning: 
+#   1. calibrate
+#   2. test distortion correction
+#   3. step 3 - 6 on an image
+#   4. run step 3 to 6 on a video
+# -------------------------------------------------------------
+    
 import step_1_calibrate as cal
 import step_2_distortion_correction as dist_co
 
 print(1)
 
-cal.test99()
-dist_co.test99()
+
